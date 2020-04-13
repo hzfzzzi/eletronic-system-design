f3251afc3ba22ba1429a2849ce0fd21f6d816b4c
Hzfzzzi <540037737@qq.com>
2020-04-13 15:19:59 +0800

    first commit

---
+++ data_get.py
@@ -0,0 +1,46 @@
+import cv2
+import os
+def get_user_data(pic_number = 30,path = 'dataset'):
+    cam = cv2.VideoCapture(0)
+    cam.set(3, 640)  # set video width
+    cam.set(4, 480)  # set video height
+
+    face_detector = cv2.CascadeClassifier('haarcascade_frontalface_alt2.xml')
+
+    # For each person, enter one numeric face id
+    face_id = input('\n enter user id end press <return> ==>  ')
+
+    print("\n [INFO] Initializing face capture. Look the camera and wait ...")
+    # Initialize individual sampling face count
+    count = 0
+
+    while (True):
+        ret, img = cam.read()
+        #img = cv2.flip(img, -1)  # flip video image vertically
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = face_detector.detectMultiScale(gray, 1.3, 5)
+
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            count += 1
+
+            # Save the captured image into the datasets folder
+            #testing under the Windows ,please adjust before transplant
+            cv2.imwrite("dataset//User." + str(face_id) + '.' + str(count) + ".jpg", gray[y:y + h, x:x + w])
+
+            cv2.imshow('image', img)
+
+        k = cv2.waitKey(100) & 0xff  # Press 'ESC' for exiting video
+        if k == 27:
+            break
+        elif count >= pic_number:  # Take 30 face sample and stop video
+            break
+
+    # Do a bit of cleanup
+    print("\n [INFO] Exiting Program and cleanup stuff")
+    cam.release()
+    cv2.destroyAllWindows()
+def main():
+    get_user_data(pic_number=300)
+if __name__ =='__main__':
+    main()
